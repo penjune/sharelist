1b05c16b4e4c6d4054f54785ac7e29467421375e
penjune <penjune@gmail.com>
2019-09-17 11:37:07 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,17 +2,6 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-echo "+============================================================+"
-echo "|                    ShareList Installer                     |"
-echo "|                                                            |"
-echo "|                                         <reruin@gmail.com> |"
-echo "|------------------------------------------------------------|"
-echo "|                                         https://reruin.net |"
-echo "+============================================================+"
-echo ""
-
-echo -e "\n|  ShareList is updating ... "
-
 # deps
 if [ -n "$(command -v apt-get)" ]
 then
@@ -22,8 +11,6 @@ then
   yum install -y wget unzip >/dev/null 2>&1
 fi
 
-
-echo -e "|\n|  Download ShareList Package ... "
 wget -O sharelist-master.zip https://github.com/reruin/sharelist/archive/master.zip >/dev/null 2>&1
 
 unzip -q -o sharelist-master.zip -d ./
@@ -31,8 +18,5 @@ unzip -q -o sharelist-master.zip -d ./
 cp -rf sharelist-master/* ./
 rm -rf sharelist-master*
 
-echo -e "|\n|  Install Dependents ... "
 npm install >/dev/null 2>&1
 pm2 restart all >/dev/null 2>&1
-
-echo -e "|\n|  Success: ShareList has been updated\n"
