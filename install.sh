693b20464cae1c5a3d3332e0e689f641c60f88f5
penjune <penjune@gmail.com>
2019-09-17 11:39:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,6 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-echo "+============================================================+"
-echo "|                    ShareList Installer                     |"
-echo "|                                                            |"
-echo "|                                         <reruin@gmail.com> |"
-echo "|------------------------------------------------------------|"
-echo "|                                         https://reruin.net |"
-echo "+============================================================+"
-echo ""
-
-echo -e "\n|   ShareList is installing ... "
-
 # deps
 if [ -n "$(command -v apt-get)" ]
 then
@@ -32,5 +21,3 @@ npm install pm2 -g
 pm2 start app.js
 pm2 save
 pm2 startup
-
-echo -e "|\n|   Success: ShareList has been installed\n|"
